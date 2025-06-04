f4ca0ccf29ab16c1f56e143f38422e7de686f506
MIKY226 <michaelclaver14@gmail.com>
2025-06-04 11:53:12 +0000

    Add next.config.js 1749037978234

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig;
